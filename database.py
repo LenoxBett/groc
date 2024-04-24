557f7145a9f228e898c2f53239ee32cd885fac32
Lenox Bett <keringlenox@gmail.com>
2024-04-24 16:42:31 +0300

    first commit

---
+++ database.py
@@ -0,0 +1,94 @@
+import psycopg2
+
+# connecting to the postgresql database
+conn = psycopg2.connect( 
+    dbname = "myduka",
+    user = "postgres",
+    password = "5555",
+    host = "localhost",
+    port = 5432,
+)
+# open a cursor to perform database operation
+
+cur = conn.cursor()
+
+def get_products():  
+    prods = cur.execute("select * from products;")
+    prods = cur.fetchall()  
+    return prods
+    # print(prods)
+    # for i in prods:
+    #     print(i)
+prods = get_products()
+print(prods)
+
+sal = conn.cursor()
+
+def get_sales():
+    sales = sal.execute("select * from sales;")
+    sales = sal.fetchall()
+    # for i in sales:
+    #     print(i)
+        
+
+get_sales()
+  
+def get_data(table):
+    select = f"select * from {table}"
+    cur.execute(select)
+    data = cur.fetchall()
+    # for i in data:
+    #     print(i)
+
+# get_data("products")
+# get_data("sales")
+
+# function to insert products
+        
+# def insert_products(values):
+#     insert = f"insert into products(name,selling_
+#     price,buying_price
+#     ,stock_quantity)values{values}"
+#     cur.execute(insert)
+#     conn.commit()
+# product_value = ("milk",50,20,3)
+# insert_products(product_value)
+
+# get_data('products')
+# get_data('sales')
+
+
+def insert_products(values):
+    insert = """insert into products(name,selling_price,
+    buying_price
+    ,stock_quantity)values(%s,%s,%s,%s)"""
+    cur.execute(insert,values)
+    conn.commit()
+product_value = ("cookies",50,20,3)
+insert_products(product_value)
+
+# get_data('products')
+# get_data('sales') 
+
+# create a function to insert sales (2 ways)
+
+def insert_sales(values):
+    insert = f"insert into sales(pid,quantity,created_at)values{values}"
+    sal.execute(insert)
+    conn.commit()
+sales_value = (1,20,"now()")
+insert_sales(sales_value)
+
+get_data('products')
+get_data('sales') 
+
+def insert_sales(values):
+    insert = """insert into sales(pid,quantity,created_at
+    )values(%s,%s,%s)"""
+    sal.execute(insert,values)
+    conn.commit()
+sales_value = (1,90,"now()")
+insert_sales(sales_value)
+
+get_data('products')
+get_data('sales')
---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask,render_template
+
+# flask instance
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello Bett"
+
+@app.route("/home")
+def home():
+    return "Hello Lynne"
+
+# products render a products.html file
+@app.route("/products")
+def products():
+    return render_template("products.html")
+
+# dashboard and render
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/sales")
+def sales():
+    return render_template("sales.html")
+
+
+
+app.run()
